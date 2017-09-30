d110deb842a76cce3b1237b1dfdff1d22dd9ecec
brwitten <bethrwitten@gmail.com>
2017-09-29 17:54:51 -0700

    on click click square displays image

--- js/app.js
+++ js/app.js
@@ -11,9 +11,13 @@ $(document).ready(function() {
 console.log("between functions");
 
   function popUpYay(event){
-    alert('Yay!');
-    $('#a1').css("background-color", "orange");
-
+    $('#a1').css("background-image", "url('images/moustache-icon.png')");
+    $('#a1').css("background-color", "gray");
+    $('#a1').css("background-repeat", "no-repeat");
+    $('#a1').css("background-position", "center");
+    console.log($('#a1').css("background-image"));
   }
+
+// $('myOjbect').css('background-image', 'url(' + imageUrl + ')');
 
 console.log("end of text");
