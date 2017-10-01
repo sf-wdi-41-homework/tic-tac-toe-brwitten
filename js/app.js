b9c4f02ac7cd6592fdf6157c9599d86578a38449
brwitten <bethrwitten@gmail.com>
2017-10-01 16:10:29 -0700

    added initial winning conditions

--- js/app.js
+++ js/app.js
@@ -16,34 +16,41 @@ $(document).ready(function() {
 });
 
 var isPlayerOnesTurn = true;
+var haveAWinner = false;
+var boardGame = {};
 
-var boardGame = [];
-
-// whenever a square is clicked, check to see if the square is in boardGame
-// if the square is in boardGame, break
-// if the square is not in boardGame, display image of current turn
-// pop square into boardGame object
 // check to see if there is a winner or no more empty spaces
-
-
 
 // function to update square image to icon
   function imageAppear(event){
-    // using the event listener to the id of the sqaure that was selected
-    var id = event.currentTarget;
-    // if / else to input correct image and update whose turn it is
-    if (isPlayerOnesTurn == true) {
-      $(id).css("background-image", "url('images/coffee.png')");
-      isPlayerOnesTurn = false;
+    var id = event.currentTarget; // identifying DOM object that was clicked
+    var square = event.currentTarget.id; // storing specific ID that was clicked
+    // checking to see if the square that was clicked is listed in object
+    if (boardGame[square] != undefined) {
+      return;
     } else {
-      $(id).css("background-image", "url('images/beer.jpg')");
-      isPlayerOnesTurn = true;
+      // if / else to input correct image and update whose turn it is
+      if ((isPlayerOnesTurn == true) && (haveAWinner == false)) {
+        boardGame[square] = "player1";
+        $(id).css("background-image", "url('images/coffee.png')");
+        isPlayerOnesTurn = false;
+      } else if ((isPlayerOnesTurn == false) && (haveAWinner == false)) {
+        boardGame[square] = "player2";
+        $(id).css("background-image", "url('images/beer.jpg')");
+        isPlayerOnesTurn = true;
+      };
+      // center the image in the square and no tiling
+      $(id).css("background-repeat", "no-repeat");
+      $(id).css("background-position", "center");
+    }
+    if ((boardGame["a1"] == boardGame["a2"]) && (boardGame["a2"] == boardGame["a3"])) {
+      alert("We have a winner");
+      haveAWinner = true;
+      return;
     };
-    // center the image in the square and no tiling
-    $(id).css("background-repeat", "no-repeat");
-    $(id).css("background-position", "center");
-    boardGame.push(id);  // adding square id to the board game array
-    console.log(isPlayerOnesTurn);
+    // console.log(boardGame["a1"]);
+    // console.log(boardGame["a2"]);
+    // console.log(boardGame["a3"]);
   }
 
 
